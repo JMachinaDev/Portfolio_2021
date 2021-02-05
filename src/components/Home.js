b272e04814d7fa672ebd30a125cce107fe6fc45b
Josue <jmachinadev@gmail.com>
2021-02-05 16:30:22 -0500

    reformat home component

--- src/components/Home.js
+++ src/components/Home.js
@@ -6,6 +6,8 @@ import { gsap } from "gsap";
 
 // TODO: convert skills to array and map
 
+const skills = ['HTML','CSS','JavaScript','Node.js','React /React Libraries','Responsive Web Design','Headlesss CMS','SEO','CSS Preprocessors/Sass/Tailwind','Bootstrap','JQuery']
+
 export default function Home () {
 
     useEffect (() => {
@@ -20,85 +22,70 @@ export default function Home () {
 
     return (
     <div className="aesthetic container-home">
-        
-        <header className="flex absolute justify-center items-center font-semibold ">
-                <h1 className='container-heading'>Welcome.</h1>
-        </header>
+      <header className="flex absolute justify-center items-center font-semibold ">
+        <h1 className='container-heading'>Welcome.</h1>
+      </header>
+      <div className="gradient-circle-one" id="animation"></div>
+      <div className="gradient-circle-two" id="animation"></div>
 
-        <div className="gradient-circle-one" id="animation"></div>
-        <div className="gradient-circle-two" id="animation"></div>
+      <main>
+        <section className="container-glass" id='animation'>
+          <span className="work-status seq">Open For Work</span>
+          <img id="img-profile" alt=""/>
 
+          <section className="card-top-section seq font-semibold">
+            <h2>I'm Josue.</h2>
+            <p className="text-lg">Front End Web Developer</p>
+            <Link to="/contact"> 
+              <button className="contact-button mt-2">Message</button>
+            </Link>
+          </section>
 
-        <main>
-            <section className="container-glass" id='animation'>
-                
-                <span className="work-status seq">Open For Work</span>
-                <img id="img-profile" alt=""/>
+          <section className="card-bottom-section seq mt-4">
+            <h6 className="font-bold text-lg">Front End Skills</h6>
 
-                <section className="card-top-section seq font-semibold">
-                    <h2>I'm Josue.</h2>
-                    <p className="text-lg">Front End Web Developer</p>
+            {skills.map((item) => (
+              <ul>
+                <li>{item}</li>
+              </ul>
+            ))}
 
-                    <Link to="/contact"> 
-                        <button className="contact-button mt-2">Message</button>
-                    </Link>
-                </section>
-
-                <section className="card-bottom-section seq mt-4">
-                    <h6 className="font-bold text-lg">Front End Skills</h6>
-                    <ul>
-                        <li>HTML</li>
-                        <li>CSS</li>
-                        <li>JavaScript</li>
-                        <li>Node</li>
-                        <li>React</li>
-                        <li>Responsive Web Design</li>
-                        <li>Headless CMS</li>
-                        <li>SEO</li>
-                        <li>Sass</li>
-                        <li>Tailwind</li>
-                        <li>React BootStrap</li>
-                        <li>JQuery</li>
-                    </ul>
-                
-                    <div className="social-section">
-                        <SocialIcon 
-                            url="https://www.linkedin.com/in/josue-mendez-0a78611ba/"
-                            target="_blank"
-                            // fgColor="#eeeeee"
-                            style={{ height: 45, width: 45}}
-                        />
-                        <SocialIcon 
-                            url="https://github.com/JMachinaDev"
-                            target="_blank"
-                            // fgColor="#eeeeee"
-                            style={{ height: 45, width: 45}}
-                        />
-                        <SocialIcon 
-                            url="https://twitter.com/JMachinaDev"
-                            target="_blank"
-                            fgColor="#eeeeee"
-                            style={{ height: 45, width: 45}}
-                        />
-                        <SocialIcon 
-                            url="mailto:jmachinadev@gmail.com"
-                            target="_blank"
-                            fgColor="#eeeeee"
-                            style={{ height: 45, width: 45}}
-                        />
-                    </div>
+            <div className="social-section">
+              <SocialIcon 
+                url="https://www.linkedin.com/in/josue-mendez-0a78611ba/"
+                target="_blank"
+                // fgColor="#eeeeee"
+                style={{ height: 45, width: 45}}
+              />
+              <SocialIcon 
+                url="https://github.com/JMachinaDev"
+                target="_blank"
+                // fgColor="#eeeeee"
+                style={{ height: 45, width: 45}}
+              />
+              <SocialIcon 
+                url="https://twitter.com/JMachinaDev"
+                target="_blank"
+                fgColor="#eeeeee"
+                style={{ height: 45, width: 45}}
+              />
+              <SocialIcon 
+                url="mailto:jmachinadev@gmail.com"
+                target="_blank"
+                fgColor="#eeeeee"
+                style={{ height: 45, width: 45}}
+              />
+            </div>
             
-                        <a className="buy-coffee" href="https://www.buymeacoffee.com/jmachinadev" target="_blank" rel="noreferrer">
-                            <img 
-                                src="https://img.buymeacoffee.com/button-api/?text=Buy me a coffee&emoji=&slug=jmachinadev&button_colour=5F7FFF&font_colour=1b222b&font_family=Poppins&outline_colour=000000&coffee_colour=FFDD00"
-                                alt="Coffee Button"
-                            />
-                        </a>
-                  
-
-                </section>
-            </section>
-        </main>
+            <a className="buy-coffee" href="https://www.buymeacoffee.com/jmachinadev" target="_blank" rel="noreferrer">
+              <img 
+                src="https://img.buymeacoffee.com/button-api/?text=Buy me a coffee&emoji=&slug=jmachinadev&button_colour=5F7FFF&font_colour=1b222b&font_family=Poppins&outline_colour=000000&coffee_colour=FFDD00"
+                alt="Coffee Button"
+              />
+            </a>
+          </section>
+        </section>
+      </main>
     </div>
-    )
+  )
 }
