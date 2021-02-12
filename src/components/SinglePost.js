5a35e895511d8c9d12cec92c496efb7bcc96999e
jmachinadev <jmachinadev@gmail.com>
2021-02-12 14:41:31 -0500

    add css style file, minor formatting changes

--- src/components/SinglePost.js
+++ src/components/SinglePost.js
@@ -4,6 +4,7 @@ import sanityClient from "../client.js";
 import imageUrlBuilder from "@sanity/image-url";
 import BlockContent from "@sanity/block-content-to-react";
 import '../css/animations.scss';
+import '../css/singlePostStyle.css';
 
 const builder = imageUrlBuilder(sanityClient);
 function urlFor(source){
@@ -39,35 +40,30 @@ export default function SinglePost () {
     <main className="bg-gray-900 min-h-screen p-12">
       <article className="container shadow-lg mx-auto bg-gray-300 rounded-lg">
         <header className="relative">
-          <div className="absolute h-full w-full flex items-center justify-center p-8">
-            <div id="singlePost-card">
-              <h1 className="cursive text-3xl lg:text-6xl mb-4 text-white">
-                  {singlePost.title}
-              </h1>
-              <div className="flex justify-center ">
-                  <img src={urlFor(singlePost.authorImage).url()} 
-                  alt={singlePost.name}
-                  className="w-10 h-10 rounded-full"
-                  />
-              </div>
-              <p className="cursive text-center px-2 text-1xl text-white">
-                  {singlePost.name}
-              </p>
-            </div>
+          <div className="absolute h-full w-full">
+            {/* <div id="singlePost-card"></div> */}
           </div>
-          <img 
-              src={singlePost.mainImage.asset.url} 
-              alt={singlePost.title}
-              className="w-full object-cover rounded-t"
-              style={{height: '400px'}}
+          <img
+            src={singlePost.mainImage.asset.url} 
+            alt={singlePost.title}
+            className="w-full object-cover rounded-t"
+            style={{height: '400px'}}
           />
         </header>
-        <div className="p-10 lg:px-48 py-12 lg:py-20 prose lg:prose-xl max-w-full text-black aesthetic">
-            <BlockContent  
-                blocks={singlePost.body} 
-                projectId="3ftsgi69" 
-                dataset="production"
+        <div className="article light-theme p-10 lg:px-14 py-12 lg:py-12 lg:prose-lg max-w-full rounded">
+          <h1 className="article article-header text-3xl lg:text-6x">{singlePost.title}</h1>
+          <div className="article-header-container">
+            <img src={urlFor(singlePost.authorImage).url()} 
+              alt={singlePost.name}
+              className="float-left m-0 w-10 h-10 rounded-full"
             />
+            <p className="cursive text-left mx-2 text-1xl">{singlePost.name}</p>
+          </div>
+          <BlockContent
+            blocks={singlePost.body} 
+            projectId="3ftsgi69" 
+            dataset="production"
+          />
         </div>
       </article>
     </main>
