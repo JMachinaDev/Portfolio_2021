539882b09bfbd6cc56c30c618d2142c320da793b
jmachinadev <jmachinadev@gmail.com>
2021-02-12 14:08:07 -0500

    create class object to fetch author/author data, minor formatting changes

--- src/components/About.js
+++ src/components/About.js
@@ -1,56 +1,50 @@
 import React, { useEffect, useState } from 'react';
-import sanityClient from '../client.js';
-// import  backgroundImage from '../images/backgroundImage.jpg';
-import profileImage from '../images/profileImage.jpg'
-import BlockContent from "@sanity/block-content-to-react"
-
+import { Client } from '../services/FetchClient';
+import profileImage from '../images/profileImage.jpg';
+import BlockContent from "@sanity/block-content-to-react";
+import '../css/aboutStyle.css';
 
 export default function About () {
-    const [author, setAuthor] =useState(null);
+  const [author, setAuthor] =useState(null);
 
-    useEffect(() =>{
-        sanityClient.fetch(`*[_type == "author"]{
-            bio,
-            "authorImage": image.asset-url
-        }`)
-        .then((data) => setAuthor(data[0]))
-        .catch(console.error);
-    }, []);
+  const initializeData = () => {
+    const client = new Client();
+    client.fetchAuthor()
+    .then((data) => {
+      setAuthor(data[0])
+    })
+    .catch(console.error);
+  }
+  useEffect(initializeData, []);
 
-    if(!author) return <div className="bg-gray-900 text-gray-100 w-full h-full absolute flex justify-center">Loading...</div>;
-    
-
-    return (
-        <main className="aesthetic dark-theme absolute w-full h-full">
-            <div className="p-10 container mx-auto relative">
-                <section className="med-theme rounded-lg shadow-2xl p-8">
-
-                    <h1 className="text-6xl font-bold mb-4">
-                        Hey there. I'm{" "}
-                        <span className="text-blue">Josue Mendez</span>
-                    </h1>
-
-                    <div className="inline relative">
-                        <img 
-                            src={profileImage}
-                            className="object-cover max-h-72 w-full mt-2 mb-4 rounded"
-                            // object-cover w-full h-auto lg:w-3/4 lg:h-64 mr-8
-                            alt={author.name}
-                        />
-                    </div>
-
-                    <div className="text-lg flex flex-col justify-center">
-                        
-                        <div className="text-base font-semibold text-gray-300 prose lg:prose-xl">
-                            <BlockContent 
-                                blocks={author.bio}
-                                projectId="3ftsgi69"
-                                dataset="production"
-                            />
-                        </div>
-                    </div>
-                </section>
-            </div>
-        </main>
-    )
+  if(!author) return <div className="bg-gray-900 text-gray-100 w-full h-full absolute flex justify-center">Loading...</div>;
+  
+  return (
+    <main className="aesthetic dark-theme absolute w-full h-full pt-24">
+      <div className="p-10 container mx-auto relative">
+        <section className="about-container med-theme rounded-lg shadow-2xl p-8">
+         <header className="about-header">
+            <h1 className="text-6xl font-bold mb-4">Hey there. I'm{" "}
+              <span className="text-blue">Josue Mendez</span>
+            </h1>
+          </header>
+          <div className="about-image">
+            <img
+              src={profileImage}
+              className="object-cover max-h-full max-w-full mt-2 mb-4 rounded"
+              alt={author.name}
+            />
+          </div>
+          <div className="about-content text-lg">
+              <BlockContent
+                className="text-base font-semibold text-gray-300 prose lg:prose-xl"
+                blocks={author.bio}
+                projectId="3ftsgi69"
+                dataset="production"
+              />
+          </div>
+        </section>
+      </div>
+    </main>
+  )
 }
